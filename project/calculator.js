6fb4ad061fa5e8b7e49e2b53f2d94242305db7b0
Uppi0910 <108005066+Uppi0910@users.noreply.github.com>
2025-07-30 22:45:31 +0700

    Update calculator.js

--- project/calculator.js
+++ project/calculator.js
@@ -1,1 +1,43 @@
+/ получаем первое и второе число
+function calculate() {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  const operator = document.getElementById("operator").value;
+  let result;
 
+  if (isNaN(num1) || isNaN(num2)) {
+    document.getElementById("result").innerText = "Ошибка! Введите числа";
+    return;
+  }
+// смотрим, что было в переменной с действием, и действуем исходя из этого
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      if (num2 === 0) {
+        document.getElementById("result").innerText = "Ошибка! Деление на ноль";
+        return;
+      }
+      result = num1 / num2;
+      break;
+    default:
+      document.getElementById("result").innerText = "Ошибка! Неизвестная операция";
+      return;
+  }
+  document.getElementById("result").innerText = "Результат: " + result;
+}
+// смена знака первого числа
+function changeSign() {
+    const numInput = document.getElementById("num1");
+    let currentValue = parseFloat(numInput.value);
+    if(!isNaN(currentValue)) {
+        numInput.value = -currentValue;
+    }     
+}
